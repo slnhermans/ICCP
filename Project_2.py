7e05f8cd6ced4013fbdef6cec42ecc6c731a8df3
ecgerritse <ecgerritse@gmail.com>
2015-03-12 14:53:10 +0100

    Updated code

--- Project_2.py
+++ Project_2.py
@@ -1,6 +1,7 @@
 #Delft University of Technology
 #International Course in Computational Physics
-#Assignment 2:
+#Assignment 2: Ising model
 #Authors: Emma C. Gerritse and Sophie L. N. Hermans
 
 #Definition of parameters
+N = 10     #Number of spin sites
