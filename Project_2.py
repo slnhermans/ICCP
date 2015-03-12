3cf77f1f91c34ad5af78f5ad05724c018da8f558
ecgerritse <ecgerritse@gmail.com>
2015-03-12 16:00:53 +0100

    Sync this!

--- Project_2.py
+++ Project_2.py
@@ -2,6 +2,30 @@
 #International Course in Computational Physics
 #Assignment 2: Ising model
 #Authors: Emma C. Gerritse and Sophie L. N. Hermans
+import numpy as np
 
 #Definition of parameters
-N = 10     #Number of spin sites
+n = 10       #Number of spin sites in one direction
+N = n**2     #Number of spin sites
+J = 1.       #Coupling constant
+kb = 1.      #Boltzmann constant
+h = 0.       #External magnetic field
+T = 293.     #Temperature: low for T<J/4
+
+#Fill an array uniform random with up and down (-1 and 1) spins
+S_init = np.random.choice([-1,1],size=(n,n),p=[0.5,0.5])
+
+#Calculate the total energy (as efficiently as possible!!!)
+def E_total():
+    
+    return E_total
+
+#Calculate the total magnetization
+def M_total(S,N):
+    M_total = np.sum(S)/(N*1.)
+    return M_total
+
+print(M_total(S_init,N))
+#Implement periodic boundary conditions
+
+#Plot magnetization as a function of k_b*T/J
